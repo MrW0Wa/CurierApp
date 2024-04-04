1b39909cbf643d430127b17655cd5ca78f55b4d3
MrW0Wa <vovaxme@gmail.com>
2024-04-04 18:05:28 +0300

    profile cards and avatar

--- app/src/main/java/com/example/courierapp/ui/screens/Profile.kt
+++ app/src/main/java/com/example/courierapp/ui/screens/Profile.kt
@@ -1,19 +1,38 @@
 package com.example.courierapp.ui.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.courierapp.R
 import com.example.courierapp.ui.elements.MyBottomBar
 import com.example.courierapp.ui.elements.MyCheckbox
+import com.example.courierapp.ui.theme.Black
 import com.example.courierapp.ui.theme.BlueMain
+import com.example.courierapp.ui.theme.Red
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -24,6 +43,67 @@ fun Profile(navController: NavController) {
         },
         containerColor = Color.White
     ) {
+
+    }
+}
+
+@Preview
+@Composable
+fun prev() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.TopCenter,
+    ){
+
+        Column(
+            modifier = Modifier.fillMaxWidth(0.85f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ){
+            Spacer(modifier = Modifier.height(20.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(painter = painterResource(id = R.drawable.moon), contentDescription = null, tint = Color.Black)
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(
+                        text = "Dark Mode",
+                        color = Black,
+                        fontWeight =  FontWeight.Medium,
+                        fontSize = 16.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(painter = painterResource(id = R.drawable.logout), contentDescription = null, tint = Red)
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(
+                        text = "Log Out",
+                        color = Black,
+                        fontWeight =  FontWeight.Medium,
+                        fontSize = 16.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Image(painter = painterResource(id = R.drawable.avtr), contentDescription = null,
+                modifier = Modifier.width(143.dp), contentScale = ContentScale.Crop)
+
+            Text(
+                text = "Hello Ken",
+                color = Color.Black,
+                fontWeight =  FontWeight.Black,
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+            )
+
+            }
 
     }
 }
